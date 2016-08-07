9eee77113c2456400a766e4c7fb9be273322324f
Benjamin Bengfort <benjamin@bengfort.com>
2016-08-06 21:33:42 -0400

    porting some tests over

---
+++ tests/test_utils/__init__.py
@@ -0,0 +1,18 @@
+# tests.test_utils
+# Testing package for the utilities module.
+#
+# Author:   Benjamin Bengfort <bbengfort@districtdatalabs.com>
+# Created:  Sat Aug 06 21:29:13 2016 -0400
+#
+# Copyright (C) 2016 District Data Labs
+# For license information, see LICENSE.txt
+#
+# ID: __init__.py [] benjamin@bengfort.com $
+
+"""
+Testing package for the utilities module.
+"""
+
+##########################################################################
+## Imports
+##########################################################################
