c26518485f7a1d568631a4c6258da35b22351efb
Benjamin Bengfort <benjamin@bengfort.com>
2016-08-18 11:22:15 -0400

    simple manifest command

--- minke/console/app.py
+++ minke/console/app.py
@@ -35,6 +35,7 @@ COMMANDS    = [
     DescribeCommand,
     PreprocessCommand,
     SizesCommand,
+    ManifestCommand,
 ]
 
 ##########################################################################
